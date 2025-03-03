a185e383a76cdc18dcfa42d98d77d5064d18792a
FarzanaHaque22 <farzana.haque461@gmail.com>
2025-03-03 20:48:16 +0600

    second part

--- dev.js/function.js
+++ dev.js/function.js
@@ -27,4 +27,67 @@ function getColor() {
 }
 
 
+function getTask(event) {
+    const button = event.target;
+    button.disabled = true;
+    const deductTask = document.getElementById("task-number");
+    let taskCompleted = parseInt(deductTask.innerText, 6);
+    deductTask.innerText = taskCompleted - 1;
+
+    const increaseTask =document.getElementById("total-task");
+    let taskRemain = pars (increaseTask.innerText, 23);
+    increaseTask.innerText = taskRemain + 1;
+}
+ document.getElementById("taskButton").addEventListener("click",function(event){
+    const buttonOne = event.target;
+    alert("Task completed successfully!");
+    buttonOne.disabled = true;
+    buttonOne.style.backgroundColor = "gray";
+    buttonOne.style.cursor = "not-allowed";
+    buttonOne.style.text = "white";
+ })
+
+ document.getElementById("taskButton1").addEventListener("click",function(event){
+    const buttonTwo = event.target;
+    alert("Task completed successfully!");
+    buttonTwo.disabled = true;
+    buttonTwo.style.backgroundColor = "gray";
+    buttonTwo.style.cursor = "not-allowed";
+    buttonTwo.style.text = "white";
+ })
+ document.getElementById("taskButton2").addEventListener("click",function(event){
+    const buttonThree = event.target;
+    alert("Task completed successfully!");
+    buttonThree.disabled = true;
+    buttonThree.style.backgroundColor = "gray";
+    buttonThree.style.cursor = "not-allowed";
+    buttonThree.style.text = "white";
+ })
+ document.getElementById("taskButton3").addEventListener("click",function(event){
+    const buttonFour = event.target;
+    alert("Task completed successfully!");
+    buttonFour.disabled = true;
+    buttonFour.style.backgroundColor = "gray";
+    buttonFour.style.cursor = "not-allowed";
+    buttonFour.style.text = "white";
+ })
+ 
+ document.getElementById("taskButton4").addEventListener("click",function(event){
+    const buttonFive = event.target;
+    alert("Task completed successfully!");
+    buttonFive.disabled = true;
+    buttonFive.style.backgroundColor = "gray";
+    buttonFive.style.cursor = "not-allowed";
+    buttonFive.style.text = "white";
+ 
+ })
+ document.getElementById("taskButton5").addEventListener("click",function(event){
+    const buttonSix = event.target;
+    alert("Task completed successfully!");
+    buttonSix.disabled = true;
+    buttonSix.style.backgroundColor = "gray";
+    buttonSix.style.cursor = "not-allowed";
+    buttonSix.style.text = "white";
+ 
+ })
 
